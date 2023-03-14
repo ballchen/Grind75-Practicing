474fabeafdc1574d4265c34e7f5095a82c758963
ball chen <ball@unh3o.com>
2023-03-14 21:36:18 +0800

    Responsive

--- src/components/Grind75.vue
+++ src/components/Grind75.vue
@@ -80,62 +80,60 @@ function random({easy=0, medium=2, hard=0}) {
 
 <template>
   <div class="container">
-    <div class="grind75">
-      <h1>Grind 75</h1>
-      <div class="d-flex mt-2">
-        <div class="px-2">
-          <div>
-            <h3>Easy</h3>
-          </div>
-          <div>
-            <input type="number" v-model="select.easy" />
-          </div>
+    <h1>Grind 75</h1>
+    <div class="d-flex flex-wrap mt-2">
+      <div class="px-2">
+        <div>
+          <h3>Easy</h3>
         </div>
-        <div class="px-2">
-          <div>
-            <h3>Medium</h3>
-          </div>
-          <div>
-            <input type="number" v-model="select.medium" />
-          </div>
+        <div>
+          <input type="number" v-model="select.easy" />
         </div>
-        <div class="px-2">
-          <div>
-            <h3>Hard</h3>
-          </div>
-          <div>
-            <input type="number" v-model="select.hard" />
-          </div>
+      </div>
+      <div class="px-2">
+        <div>
+          <h3>Medium</h3>
         </div>
+        <div>
+          <input type="number" v-model="select.medium" />
+        </div>
+      </div>
+      <div class="px-2">
+        <div>
+          <h3>Hard</h3>
+        </div>
+        <div>
+          <input type="number" v-model="select.hard" />
+        </div>
+      </div>
+    </div>
+    
+    <div class="mt-4 d-flex justify-content-center">
+      <div class="px-2" v-if="!counting">
+        <button type="button" class="btn btn-primary" @click="random(select)">Generate Questions</button>
+      </div>
+      <div class="px-2" v-if="questions.length && seconds > 0 && !counting" type="button">
+        <button class="btn btn-warning" @click="startTimer()">Start!</button>
+      </div>
+      <div class="px-2" v-if="counting">
+        <button type="button" class="btn btn-danger" @click="endTimer()">Stop!</button>
+      </div>
+    </div>
+
+    <div class="mt-3 text-center">
+      <h2 v-if="seconds > 0" :class="{ 'text-danger': counting }">{{ formatSeconds(seconds) }}</h2>
+    </div>
+    
+
+
+    <div class="question-section mt-3" v-for="question in questions" :key="question.id">
+      <div class="question-title">
+        <a class="fs-4 fw-semibold" :href="question.link" target="_blank">{{ question.title }}</a> 
+      </div>
+      <div class="question-difficulty">
+        <p class="text-muted fw-semibold">{{ question.difficulty }}</p>
       </div>
       
-      <div class="mt-2 d-flex justify-content-center">
-        <div class="px-2" v-if="!counting">
-          <button type="button" class="btn btn-primary" @click="random(select)">Generate Questions</button>
-        </div>
-        <div class="px-2" v-if="questions.length && seconds > 0 && !counting" type="button">
-          <button class="btn btn-warning" @click="startTimer()">Start!</button>
-        </div>
-        <div class="px-2" v-if="counting">
-          <button type="button" class="btn btn-danger" @click="endTimer()">Stop!</button>
-        </div>
-      </div>
-
-      <div class="mt-3 text-center">
-        <h2 v-if="seconds > 0" :class="{ 'text-danger': counting }">{{ formatSeconds(seconds) }}</h2>
-      </div>
-      
-
-
-      <div class="question-section mt-3" v-for="question in questions" :key="question.id">
-        <div class="question-title">
-          <a class="fs-4 fw-semibold" :href="question.link" target="_blank">{{ question.title }}</a> 
-        </div>
-        <div class="question-difficulty">
-          <p class="text-muted fw-semibold">{{ question.difficulty }}</p>
-        </div>
-        
-      </div>
     </div>
   </div>
  
